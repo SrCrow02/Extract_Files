e173f262d560e286193a2afbfa30e73297c14441
Crowler <88294753+SrCrow02@users.noreply.github.com>
2023-08-24 16:35:17 -0300

    Por enquanto apenas descompacta

---
+++ index.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import path from 'path';
+import admzip from 'adm-zip';
+import inquirer from 'inquirer';
+
+const zipPath = "./enterDir/a.zip";
+const output = "./extractFile/";
+
+const menuOptions = {
+    type: "list",
+    name: "menuItem",
+    message: "Escolha ua opção",
+    choices: [
+        'Extract',
+        'Compact',
+        'Sair'
+    ]
+}
+
+async function verifyFolder() {
+    try {
+      await fs.stat(output)
+    } catch{
+      fs.mkdir(output)
+    }
+  }
+
+async function extractFiles(){
+       
+    const zip = new admzip(zipPath);
+    
+    zip.extractAllToAsync(output, true, function (error) {
+        if (error) {
+            console.error("Erro ao extrair os arquivos:", error);
+        } else {
+            console.log("Arquivos extraídos com sucesso!");
+        }
+    });
+}
+
+async function showMenu() {
+    const answer = await inquirer.prompt(menuOptions);
+    
+    switch (answer.menuItem) {
+      case 'Extract':
+        console.log("a")
+        extractFiles()
+        break;
+      case 'Compact':
+        console.log("a")
+        break;
+      case 'Sair':
+        console.log('Saindo do menu.');
+        return;
+    }
+  }
+
+showMenu()
+
